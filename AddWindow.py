f41e01a824ce11ad71d12db88fde1ee55adc9185
matiit <matiqing@gmail.com>
2014-05-25 23:01:38 +0200

    corrected (swapped files content)

--- AddWindow.py
+++ AddWindow.py
@@ -4,21 +4,6 @@ from PySide.QtGui import *
 from PySide.QtCore import Slot
 
 
-class MainWindow(QDialog):
+class AddWindow(QDialog):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.newButton = QPushButton("Add new connection")
-        self.listWidget = QListWidget(self)
-        self.listWidget.addItem("Item_1")
-
-        self.grid = QGridLayout()
-        self.grid.addWidget(self.newButton, 0, 0)
-        self.grid.addWidget(self.listWidget, 1, 0)
-
-        self.setLayout(self.grid)
-
-        self.newButton.clicked.connect(self.clickedNewButton)
-
-    @Slot()
-    def clickedNewButton(self):
-        print("ClickedNewButton")
+        super(AddWindow, self).__init__(parent)
--- MainWindow.py
+++ MainWindow.py
@@ -4,6 +4,21 @@ from PySide.QtGui import *
 from PySide.QtCore import Slot
 
 
-class AddWindow(QDialog):
+class MainWindow(QDialog):
     def __init__(self, parent=None):
-        super(AddWindow, self).__init__(parent)
+        super(MainWindow, self).__init__(parent)
+        self.newButton = QPushButton("Add new connection")
+        self.listWidget = QListWidget(self)
+        self.listWidget.addItem("Item_1")
+
+        self.grid = QGridLayout()
+        self.grid.addWidget(self.newButton, 0, 0)
+        self.grid.addWidget(self.listWidget, 1, 0)
+
+        self.setLayout(self.grid)
+
+        self.newButton.clicked.connect(self.clickedNewButton)
+
+    @Slot()
+    def clickedNewButton(self):
+        print("ClickedNewButton")
